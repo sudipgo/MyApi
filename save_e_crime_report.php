101ae3e72bef1c2d70595a65657c1d612500bb09
sudipgo <godasesudip@gmail.com>
2023-03-04 09:42:19 +0530

    Update save_e_crime_report.php

--- save_e_crime_report.php
+++ save_e_crime_report.php
@@ -16,22 +16,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	$amount='2000';
 	$to_email_id='smitapawar926@gmail.com';
 	
-	$output='<div style="border:1px solid;width:75%;margin: auto;padding: 10px;"><div <img  src="miscos.png" alt="logo" class="miscos">style="margin:0px;padding:0px;"><h2  align="center" style="height:10px;">MISCOS TECHNOLOGIES PVT.LTD.</h2>';
-			 $output.='<p align="center"style="margin:0px;padding:0px;font-weight:bold;font-size:12px;height:20px;">Invoice For Advertisement</p></div><hr>';
+	$output='<div style="border:1px solid;width:75%;margin: auto;padding: 10px;"><div <img  src="miscos.png" alt="logo" class="miscos"><h2  align="center" style="height:10px;">MISCOS TECHNOLOGIES PVT.LTD.</h2>';
+			 $output.='<p align="center" style="margin:0px;padding:0px;font-weight:bold;font-size:12px;height:20px;margin-top:40px;">Invoice For Advertisement</p></div><hr>';
 			 $output.='<p style="font-weight:bold;font-size:12px;height:20px;float:right;padding-right:10%;">Date : '.date('d-m-Y').'</p>';
-				$output.='<table width="100%" cellspacing="5" cellpadding="5" style="margin-bottom:50px;">';
+								$output.='<table width="100%" cellspacing="5" cellpadding="5" style="margin-bottom:50px;"></table>';
 			
 				$output.='
-				
-				<table style="font-family: arial, sans-serif;border-collapse: collapse;width: 90%;margin: auto;margin-bottom:50px; border:1px solid;">
+				<div style="display:flex;"><div><table style="font-family: arial, sans-serif;border-collapse: collapse;width: 50%;margin: auto;margin-bottom:50px;margin-left:10px; border:1px solid;">
 			    <tr>
-					<td style="border: 1px solid black;text-align: left;padding: 8px;width:1%;">'.$name.'</td>
+					<td style="border: 1px solid black;text-align: left;padding: 8px;width:1%;background-color:#0064b2;color:#ffffff;">'.$name.'</td>
 					<td style="border: 1px solid black;text-align: left;padding: 8px;width:1%;">'.$mobile_no.'</td>
 					
-				</tr>';
-
+				</tr></table></div><div style="margin-left:100px;"><table style="font-family: arial, sans-serif;border-collapse: collapse;width: 50%;margin: auto;margin-bottom:50px;margin-left:10px; border:1px solid;">
+			    <tr>
+					<td style="border: 1px solid black;text-align: left;padding: 8px;width:1%;background-color:#0064b2;color:#ffffff;">Name</td>
+					<td style="border: 1px solid black;text-align: left;padding: 8px;width:1%;">Dummy </td>
+					
+				</tr></table></div></div>';
 			
-			$output .= '</table><h3 style="padding-left:50px;margin-bottom:20px;">Total Cases: '.$data->numberOfCases.'</h3><h4 style="font-family: arial, sans-serif;border-collapse: collapse;width:50px;margin: auto;margin-bottom:10px;">Case Details:</h4>
+			$output .= '<table><h3 style="padding-left:50px;margin-bottom:20px;">Total Cases: '.$data->numberOfCases.'</h3><h4 style="font-family: arial, sans-serif;border-collapse: collapse;width:50px;margin: auto;margin-bottom:10px;">Case Details:</h4>
 			<table style="font-family: arial, sans-serif;border-collapse: collapse;width: 90%;margin: auto;margin-bottom:50px;">
 			  <tr>
 				<th style="border: 1px solid black;text-align: center;padding: 8px;width:1%;">Sr.No.</th>
